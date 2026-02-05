b6f53f7fc335415a65a94daafd6fd7c91ffbd82f
Eric Reed <eric@an0n.dev>
2026-02-05 13:46:03 -0500

    flask app skeleton

---
+++ README.md
@@ -0,0 +1,6 @@
+`pip install flask`
+
+`python app.py`
+
+[http://localhost:40000](http://localhost:40000)
+
---
+++ app.py
@@ -0,0 +1,14 @@
+import flask
+
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return flask.render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port=40000, debug=True)
+
