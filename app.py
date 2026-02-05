adb2827be281eec8f6915f9537fb1a738c6aa57e
Samp <ihurtmyknee11@gmail.com>
2026-02-05 14:12:11 -0500

    added a gitignore and secondary template page

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = flask.Flask(__name__)
 def root():
     return flask.render_template("index.html")
 
+@app.route("/campdle")
+def cambpdle():
+    return flask.render_template("campdle.html")
+
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port=40000, debug=True)
